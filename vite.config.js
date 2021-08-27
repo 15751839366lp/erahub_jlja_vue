b6c47c549ac7bf3458f5dc06e381b998f6207744
李彭 <15751839366@163.com>
2021-08-27 19:39:20 +0800

    nothing

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,33 @@ export default {
     plugins: [vue()],
     optimizeDeps: {
         include: ['schart.js']
+    },
+    server: {
+        host: "127.0.0.1",
+        port: 9999,
+        proxy: {
+            // string shorthand
+            // '/foo': 'http://localhost:4567',
+            // with options
+            // '/api': {
+            //     target: 'http://jsonplaceholder.typicode.com',
+            //     changeOrigin: true,
+            //     rewrite: (path) => path.replace(/^\/api/, '')
+            // },
+            // // with RegEx
+            // '^/fallback/.*': {
+            //     target: 'http://jsonplaceholder.typicode.com',
+            //     changeOrigin: true,
+            //     rewrite: (path) => path.replace(/^\/fallback/, '')
+            // },
+            // // Using the proxy instance
+            // '/api': {
+            //     target: 'http://jsonplaceholder.typicode.com',
+            //     changeOrigin: true,
+            //     configure: (proxy, options) => {
+            //         // proxy will be an instance of 'http-proxy'
+            //     }
+            // }
+        }
     }
 }
