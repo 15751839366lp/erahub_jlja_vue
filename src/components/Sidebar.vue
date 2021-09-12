c27b8da0b22abfea8298a99b88dd1e3f42a5eba6
李彭 <15751839366@163.com>
2021-09-12 15:29:04 +0800

    nothing

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,27 +4,35 @@
                  text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
             <template v-for="item in items">
                 <template v-if="item.subs">
-                    <el-submenu :index="item.url" :key="item.index">
+                    <el-submenu :index="item.id" :key="item.id">
                         <template #title>
                             <i :class="item.icon"></i>
                             <span>{{ item.permission }}</span>
                         </template>
                         <template v-for="subItem in item.subs">
-                            <el-submenu v-if="subItem.subs" :index="subItem.url" :key="subItem.url">
+                            <el-submenu v-if="subItem.subs" :index="subItem.id" :key="subItem.id">
                                 <template #title>
+                                    <i :class="subItem.icon"></i>
                                     {{ subItem.permission }}
                                 </template>
-                                <el-menu-item v-for="(threeItem, i) in subItem.subs" :key="i" :index="threeItem.url">
-                                    {{ threeItem.permission }}
+                                <el-menu-item v-for="(threeItem, i) in subItem.subs" :key="threeItem.id" :index="threeItem.url">
+                                    <template #title>
+                                        <i :class="threeItem.icon"></i>
+                                        <span>{{ threeItem.permission }}</span>
+                                    </template>
                                 </el-menu-item>
                             </el-submenu>
-                            <el-menu-item v-else :index="subItem.url" :key="subItem.url">{{ subItem.permission }}
+                            <el-menu-item v-else :index="subItem.url" :key="subItem.id">
+                                <template #title>
+                                    <i :class="subItem.icon"></i>
+                                    <span>{{ subItem.permission }}</span>
+                                </template>
                             </el-menu-item>
                         </template>
                     </el-submenu>
                 </template>
                 <template v-else>
-                    <el-menu-item :index="item.url" :key="item.index">
+                    <el-menu-item :index="item.url" :key="item.id">
                         <i :class="item.icon"></i>
                         <template #title>{{ item.permission }}</template>
                     </el-menu-item>
